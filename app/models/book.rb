237f4d88358feb7fc30374b39e58bf91ec7ce190
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-01 09:45:09 +0000

    エラー不明

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites
-  
+  has_many :book_comments
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
