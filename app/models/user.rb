20f9465dfec620bf2fe63e9fbd0b1724a77dd9f9
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-05 07:53:07 +0000

    index

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,20 @@ class User < ApplicationRecord
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
-  has_many :following_users, through: :relationship, source: :follower
-  has_many :follower_users, through: :relationship, source: :followed
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
+  
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    following_users.include?(user)
+  end
   
   has_one_attached :profile_image
 
