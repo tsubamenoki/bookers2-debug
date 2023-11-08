b1f46ee6b70bc11884d91fa37c081caeeba90692
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-08 08:00:55 +0000

    finish search action

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,20 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      Book.where("title like ?","#{word}")
+    elsif search == "forward_match"
+      Book.where("title like ?","#{word}%")
+    elsif search == "backward_match"
+      Book.where("title like ?","%#{word}")
+    elsif search == "partial_match"
+      Book.where("title like ?","%#{word}%")
+    else
+      Book.all
+    end
+  end
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,32 +7,46 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites
   has_many :book_comments
-  
+
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
   has_many :following_users, through: :followers, source: :followed
   has_many :follower_users, through: :followeds, source: :follower
-  
+
   def follow(user_id)
     followers.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
     followers.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
     following_users.include?(user)
   end
-  
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      User.where("name like ?","#{word}")
+    elsif search == "forward_match"
+      User.where("name like ?","#{word}%")
+    elsif search == "backward_match"
+      User.where("name like ?","%#{word}")
+    elsif search == "partial_match"
+      User.where("name like ?","%#{word}%")
+    else
+      User.all
+    end
+  end
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
